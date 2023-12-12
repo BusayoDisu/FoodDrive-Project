653e79879d13d09446d79cf4c18a92506713a31b
Kaden S <52479937+superlongnameguy@users.noreply.github.com>
2023-12-12 14:13:11 -0700

    Update FoodDrive_demo_main.py

--- FoodDrive_demo_main.py
+++ FoodDrive_demo_main.py
@@ -13,7 +13,7 @@ data = pd.read_csv('combined_data.csv', encoding='latin1')
 
 # Page 1: Dashboard
 def dashboard():
-    st.image('Logo.png', use_column_width='auto')
+    st.image('Logo.png', width=500)
 
     st.subheader("💡 Abstract:")
 
